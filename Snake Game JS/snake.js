96af4570758f28061210ed4255fae03e08133bbb
yash aher <aheryash2004@gmail.com>
2024-06-17 22:22:17 +0530

    create snake

--- Snake Game JS/snake.js
+++ Snake Game JS/snake.js
@@ -1,37 +1,22 @@
+function intialization()
+{
+    myCanvas = document.getElementById("mycanvas");
+    canvasWidth = myCanvas.width = 1000;
+    canvasHeight = myCanvas.height = 866;
+}
 
-function intialization(){
-    console.log("init");
-    canvas = document.getElementById("mycanvas");
-    canvas_width = canvas.width = 500; 
-    canvas_height = canvas.height = 500;
-    pen = canvas.getContext("2d");
-    rect = {
-        x : 20,
-        y : 20,
-        w : 40,
-        h : 40,
-        speed :50
-    }
+function draw()
+{
 
 }
 
-function draw(){
-    pen.clearRect(0,0,canvas_width,canvas_height);
-    pen.fillRect(rect.x,rect.y,rect.w,rect.h);
-    pen.fillStyle = "Red";
-}
-
-function update(){
-        rect.x += rect.speed;
-        if(rect.x > canvas_width-rect.w || rect.x < 0)
-        {
-            rect.speed *= -1;
-        }
+function update()
+{
 
 }
 
-function gameLoop(){
-    console.log("gameloop");
+function gameLoop()
+{
     draw();
     update();
 }
